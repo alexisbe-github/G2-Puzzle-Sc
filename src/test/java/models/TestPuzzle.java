537f35aad80bd6372b4a0e3eec6fadd07266b2a6
alexisbe-github <alexis.beer5@etu.univ-lorraine.fr>
2023-09-11 17:04:29 +0200

    Premier commit tests
    Ajout du fichier de test unitaire pour commencer la structuration du projet.

---
+++ src/test/java/models/TestPuzzle.java
@@ -0,0 +1,14 @@
+package test.java.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPuzzle {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
