05286eb518fdcd55398ee43d5147a1326e275f6b
alexisbe-github <alexis.beer5@etu.univ-lorraine.fr>
2023-09-14 19:27:11 +0200

    update

---
+++ src/test/java/model/bdd/dao/TestCRUD.java
@@ -0,0 +1,16 @@
+package test.java.model.bdd.dao;
+
+/**
+ * Méthodes de test de l'implémentation du CRUD
+ */
+public interface TestCRUD {
+	
+	void testCreate();
+	
+	void testRead();
+	
+	void testUpdate();
+	
+	void testDelete();
+
+}
--- src/test/java/model/bdd/dao/TestDAOJoueur.java
+++ src/test/java/model/bdd/dao/TestDAOJoueur.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 
 import main.java.model.bdd.Connexion;
 import main.java.model.bdd.dao.DAOJoueur;
 
-public class TestDAOJoueur {
+public class TestDAOJoueur implements TestCRUD{
 	
 	private DAOJoueur dao;
 	private Connection con;
@@ -19,6 +20,32 @@ public class TestDAOJoueur {
 	@BeforeAll
 	public void setUp() {
 		con = Connexion.getInstance().getConnexion();
+	}
+
+	@Test
+	@Override
+	public void testCreate() {
+		
+	}
+
+	@Test
+	@Override
+	public void testRead() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Test
+	@Override
+	public void testUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void testDelete() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
