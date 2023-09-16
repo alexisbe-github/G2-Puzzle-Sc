c2801dffe2255fbb0c97c42aa43c4cb688830312
Baptiste <billardbaptiste@hotmail.fr>
2023-09-16 14:34:42 +0200

    test

---
+++ src/main/java/vue/VueLancement.java
@@ -0,0 +1,14 @@
+package main.java.vue;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VueLancement extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
