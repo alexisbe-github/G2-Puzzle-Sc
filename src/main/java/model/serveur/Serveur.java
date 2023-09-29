c9f91c53823f039c11278792e8b1add14a280030
alexisbe-github <66599552+alexisbe-github@users.noreply.github.com>
2023-09-29 15:53:24 +0200

    Update Serveur.java

--- src/main/java/model/serveur/Serveur.java
+++ src/main/java/model/serveur/Serveur.java
@@ -29,8 +29,10 @@ public class Serveur {
 			try {
 				ThreadGroup groupe = new ThreadGroup("socketsClients"); // on fait un groupe de thread pour gérer les
 																		// multiples connexion au serveur
-				serverSocket = new ServerSocket(); // on fait un serveur socket sur le port pour les
+				final ServerSocket serverSocket = new ServerSocket(); // on fait un serveur socket sur le port pour les
 														// connexions qu'on transforme en socket
+				serverSocket.setReuseAddress(true);
+				serverSocket.bind(new java.net.InetSocketAddress(port));
 				noConnexion = 0;
 
 				// on accepte les connexion sur la server socket et on incrémente le nombre de
@@ -43,7 +45,7 @@ public class Serveur {
 						ServeurThread st = new ServeurThread(clientSocket, groupe, noConnexion, partie);
 						st.start();
 					} catch (SocketException se) {
-						//this.serverOn = false;
+						this.serverOn = false;
 					}
 
 				}
