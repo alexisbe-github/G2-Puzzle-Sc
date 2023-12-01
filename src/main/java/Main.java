016417ce2265bb9544d7caad5599cf3b4bfd51f5
mi-1000 <119438442+mi-1000@users.noreply.github.com>
2023-12-01 18:54:01 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,10 @@ import main.java.vue.MainApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			if (args[0].equals("-c"))
-				new JeuConsole();
-			else
-				MainApplication.open();
-		} else {
+		if (args.length > 0 && args[0].equals("-c"))
+			new JeuConsole();
+		else
 			MainApplication.open();
-		}
-
 	}
 
 }
