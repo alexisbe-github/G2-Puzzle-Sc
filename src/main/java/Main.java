11a382063fd15de3c262d9c9ca4606309b2c182e
mi-1000 <119438442+mi-1000@users.noreply.github.com>
2023-12-01 18:56:33 +0100

    Merge branch 'main' of https://github.com/alexisbe-github/G2-Puzzle-Sc-2023

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 package main.java;
 
-import main.java.vue.MainApplication;
-
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length > 0 && args[0].equals("-c"))
-			new JeuConsole();
-		else
-			MainApplication.open();
+		if (args.length == 0) {
+			MainGUI.main(args);
+		} else {
+			if (args[0].equals("-c"))
+				new JeuConsole();
+		}
 	}
 
 }
