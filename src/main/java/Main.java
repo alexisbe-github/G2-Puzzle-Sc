bd183cd96b500988b9d6cd2b26a2f542e3e91a64
alexisbe-github <alexis.beer5@etu.univ-lorraine.fr>
2023-12-01 18:50:50 +0100

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,14 @@
 package main.java;
 
-import main.java.vue.MainApplication;
-
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length > 0) {
+		if (args.length == 0) {
+			MainGUI.main(args);
+		} else {
 			if (args[0].equals("-c"))
 				new JeuConsole();
-			else
-				MainApplication.open();
-		} else {
-			MainApplication.open();
 		}
-
 	}
 
 }
